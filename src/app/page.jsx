5898129104fdac74811acc30ea5fae82206b3480
DamianMuracciole <102468224+DamianMuracciole@users.noreply.github.com>
2024-05-06 10:13:37 -0300

    "agrego imagen de prueba en la home"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 
 export default async function Home() {
   return (
     <main>
       <div>
         <h1>HomePage</h1>
+        <Image
+          src='/images/defaultImage.png'
+          width={180}
+          height={180}
+          alt='default image'
+        />
       </div>
     </main>
   )
